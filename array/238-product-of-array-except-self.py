b03d4d20f32ac3e1863356c965a115e69ef7260a
jiyapatel4 <jiya.patel@mail.utoronto.ca>
2026-03-03 21:28:55 -0500

    238-product-of-array-except-self

---
+++ array/238-product-of-array-except-self.py
@@ -0,0 +1,47 @@
+import unittest
+class Solution():
+    def productExceptSelf(self, nums):
+        """
+        Invariant: 
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        left_arr = []
+        for i in range(len(nums)):
+            if i == 0:
+                left_arr.append(1)
+            else:
+                left_arr.append(nums[i - 1] * left_arr[i - 1])
+
+        right_val = 1
+        for j in range(len(nums) - 1, -1, -1):
+            left_arr[j] *= right_val
+            right_val *= nums[j]
+        
+        return left_arr
+    
+class TestProductExceptSelf(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def test_four_positive_numbers(self):
+        """
+        Calcualte the answer for four positive numbers:
+        [2, 3, 4, 5]
+        """
+        nums = [2, 3, 4, 5]
+        output = [60, 40, 30, 24]
+        result = self.sol.productExceptSelf(nums)
+        self.assertEqual(result, output)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+        
+
+
+
+        
+
